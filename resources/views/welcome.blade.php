99694b63ebfbcc22a5b4c7fe000038fd4ea83a88
woyanas <kanasfi.british@gmail.com>
2021-11-25 17:08:13 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -680,11 +680,11 @@
           <div class="col-lg-3 col-md-6 footer-contact">
             <h3>Kierbie Chatbot</h3>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              Jl. AMD Babakan Pocis <br>
+              Setu, Tangerang Selatan 15315<br>
+              Indonesia <br><br>
+              <strong>Phone:</strong> +6285756666660<br>
+              <strong>Email:</strong> renjana.net@gmail.com<br>
             </p>
           </div>
 
