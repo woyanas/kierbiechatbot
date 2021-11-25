0a9d3efa61dc9d79d8c6bbf0e682ee884f3ca05f
woyanas <kanasfi.british@gmail.com>
2021-11-25 11:50:27 +0700

    update chatbot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -817,15 +817,4 @@
   <script src="assets/js/main.js"></script>
 
 </body>
-
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
-    <script>
-        var botmanWidget = {
-            aboutText: 'Write Something',
-            introMessage: "✋ Hi! Saya Kierbie. Asisten Islami Anda."
-        };
-    </script>
- 
-    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
-
 </html>
