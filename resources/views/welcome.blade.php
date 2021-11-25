d4ab3ff8aa8f0f06341d124ba47c015bf2fa6484
woyanas <kanasfi.british@gmail.com>
2021-11-25 18:54:30 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -115,6 +115,11 @@
             <img src="assets/img/clients/client-6.png" class="img-fluid" alt="">
           </div>
 
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/clients/client-7.png" class="img-fluid" alt="">
+          </div>
+          
+
         </div>
 
       </div>
