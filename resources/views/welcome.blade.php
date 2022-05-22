834fcbbf7ab11c787bdc2b5141ce2eb12292cf5a
woyanas <kanasfi.british@gmail.com>
2022-05-22 14:09:32 +0700

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -732,13 +732,13 @@
       <div class="copyright">
         &copy; Copyright <strong><span>Kierbie Chatbot</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
+      <!-- <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/Morris IF 20-free-bootstrap-html-template-corporate/ -->
         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+      </div> -->
     </div>
   </footer><!-- End Footer -->
 
